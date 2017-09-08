a97a37c47ee4b887ececb70fc75bbf58fc1bc78c
mortea15 <mortea15@student.uia.no>
2017-09-08 19:31:37 +0200

    Added comments to answer task 5 fully

--- PowershellScripting/task5.ps1
+++ PowershellScripting/task5.ps1
@@ -1,11 +1,24 @@
-# Create an AES (AesManaged) Object, used by the other functions
+# This script uses AES(-256), also known as Advanced Encryption Standard or Rijndael.
+# I've used the built-in implementation of the AES algorithm, because I saw no need to reinvent the wheel.
+# Documentation: https://msdn.microsoft.com/en-us/library/system.security.cryptography.aesmanaged%28v=vs.110%29.aspx?f=255&MSPPError=-2147217396
+# This implementation uses a 256 bit key size, which is the highest value, for the highest possible level of encryption.
+# To be brief, this cipher is approved by the NSA.
+# This encryption method is an improvement over the one found in the file task1-4.ps1 because it uses a key
+# which is randomly generated and consists of multiple characters, instead of just adding or subtracting 1 from each char.
+# While it is undoubtly more secure than the encryption method mentioned above, it does have one small challenge:
+# The key which is used to encrypt the text in this implementation, is randomly generated, which means that 
+# the encrypted string can only be decrypted in the same PowerShell session.
+# It is possible to make a solution to this by setting the Initialization Vector, but I chose not to in this implementation.
+# 
+
+# Initialize an AES (AesManaged) object, used by the other functions decrypt, key, and encrypt
 function AesObject($key, $IV) {
 
 	$AES = New-Object "System.Security.Cryptography.AesManaged"
 	$AES.Mode = [System.Security.Cryptography.CipherMode]::CBC # Cipher Block Chaining
+    $AES.KeySize = 256 # Sets the key size in bits (256 is the max value)
 	$AES.Padding = [System.Security.Cryptography.PaddingMode]::Zeros # If padding is needed, zeros will be used
 	$AES.BlockSize = 128 # The unit of data that can be de- or encrypted in one operation. Padding is used if the data is less than this value
-	$AES.KeySize = 256 # Sets the key size in bits (256 is the max value)
 	if ($IV) { # IV; Initialization Vector, used along with the key
 		if ($IV.getType().Name -eq "String") {
 			$AES.IV = [System.Convert]::FromBase64String($IV) # Converts $IV if string
@@ -25,7 +38,7 @@ function AesObject($key, $IV) {
 	$AES
 }
 
-# Create a key used by the AES object
+# Generate a key used by the AES object
 function AesKey() {
 	$AES = AesObject
 	$AES.GenerateKey()
